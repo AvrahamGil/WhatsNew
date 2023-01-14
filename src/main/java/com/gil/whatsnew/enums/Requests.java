faa1f1d35c9a455364b8610694140e8f70d57890
Gil Avraham <gil9001@gmail.com>
2023-01-14 22:27:43 +0200

    README and Dockerfile modified

--- src/main/java/com/gil/whatsnew/enums/Requests.java
+++ src/main/java/com/gil/whatsnew/enums/Requests.java
@@ -4,11 +4,11 @@ public enum Requests {
 
 	RapidHost("x-rapidapi-host"),
 	RapidKey("x-rapidapi-key"),
-	NewYorkApi("NewYorkApi"),
-	RapidApi("RapidApi"),
-	RapidKeyValue("RapidKeyValue"),
+	NewYorkApi("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bPgxNsAMl6CWyxE5Ujt0DgmHVR4XowmX&q="),
+	RapidApi("https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?toPublishedDate=null&fromPublishedDate=null&withThumbnails=true&pageSize=15&autoCorrect=false&pageNumber=1&q="),
+	RapidKeyValue("c8e4e21d5cmsh0da2d214f3b6c2bp1a5e2ejsn75621a071d73"),
 	
-	ContextRapidHostValue("ContextRapidHostValue");
+	ContextRapidHostValue("contextualwebsearch-websearch-v1.p.rapidapi.com");
 	
 	String strings;
 	
