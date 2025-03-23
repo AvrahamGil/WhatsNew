1a076bf8fe3b09573879c9fd7634748e85d812fd
Gil <gil.avraham2@bankleumi.co.il>
2025-03-23 12:10:23 +0200

    Deleted docker folder

--- Docker/mongo-users.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:de0a2cda7bc3a61ab11edf531aefac10b0feb5b3a21fe3264dfc71b620d79f58
-size 404
--- Docker/startup.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9a7c79bffcc7d2fa4cf2f4fe69a91f704672dae6199bb6ebf733f1b254363fda
-size 91
